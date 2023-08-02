1a3985741fec39252b7de16b475a27861bbcb008
Arham <nasirarham656@gmail.com>
2023-08-02 04:25:18 -0700

    regiter toto model and updated model name

--- todo_app/models.py
+++ todo_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Book(models.Model):
+class ToDo(models.Model):
     id = models.AutoField
     title = models.CharField(max_length=100)
     description = models.TextField()
