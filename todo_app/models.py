f508ae44e6a772d39bba6ad564dc4df85b16d85f
Arham <nasirarham656@gmail.com>
2023-08-02 04:17:25 -0700

    Add model

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    id = models.AutoField
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateField()
+    completed = models.BooleanField()
