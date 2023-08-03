90b899a6e2c6700b7e8302580d6501ea461ebb1f
Arham <nasirarham656@gmail.com>
2023-08-03 01:08:22 -0700

    implement Get list method in view class and add url in url class

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import ToDo
+from .serializer import ToDoSerializer
+from rest_framework import status, generics
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin
 
 # Create your views here.
+class ToDoList(GenericAPIView,ListModelMixin):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request,*args,**kwargs)
+
--- todo_application/urls.py
+++ todo_application/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('apitodo/', views.ToDoList.as_view()),
+
+
 ]
