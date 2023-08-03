37ca644edc8e1a4fb5f43fcf9e8de86ebfa6c4c6
Arham <nasirarham656@gmail.com>
2023-08-03 01:06:50 -0700

    changes in models,admin,and serializer file

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import ToDo
 
-admin.site.register(ToDo)
+
+@admin.register(ToDo)
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'created_at', 'completed']
--- todo_app/models.py
+++ todo_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class ToDo(models.Model):
-    id = models.AutoField
+    # id = models.AutoField
     title = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateField()
