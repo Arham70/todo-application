a1a55fe8192ce16dd79e73b9f30001935f4ecd73
Arham <nasirarham656@gmail.com>
2023-08-07 16:09:26 -0700

    create application using functional based and error handling in this file

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,70 +2,71 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from .models import ToDo
 from .serializer import ToDoSerializer
-from rest_framework import status, generics
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import DestroyModelMixin, ListModelMixin, CreateModelMixin, RetrieveModelMixin, \
-    UpdateModelMixin
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .Exception import NotFoundException
 
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def ToDo_Api(request, pk=None):
+    if request.method =='GET':
+        id =pk
+        if id is not None:
+            try:
+                TODO = ToDo.objects.get(id=id)
+            except ToDo.DoesNotExist:
+                raise NotFoundException
+            serializer = ToDoSerializer(TODO)
+            return Response(serializer.data)
 
-# Create your views here.
-class ToDoList(GenericAPIView, ListModelMixin):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
+        TODO = ToDo.objects.all()
+        serializer = ToDoSerializer(TODO, many=True)
+        return Response(serializer.data)
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    if request.method == 'POST':
+        serializer = ToDoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg' : 'Data uploaded'}, status=status.HTTP_201_CREATED)
+        return Response({'msg' : 'Data not uploaded'}, status=status.HTTP_400_BAD_REQUEST)
 
+    if request.method == 'PUT':
+        id = pk
+        if id is not None:
+            try:
+                TODO= ToDo.objects.get(pk=id)
+            except ToDo.DoesNotExist:
+                raise NotFoundException
+            serializer = ToDoSerializer(TODO, data=request.data , partial=False)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'msg': 'Complete Data Updated'})
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'msg': 'U cannot update data because u do not put id'}, status=status.HTTP_400_BAD_REQUEST)
 
-class ToDoCreate(GenericAPIView, CreateModelMixin):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
+    if request.method == 'PATCH':
+        id = pk
+        if id is not None:
+            try:
+                TODO = ToDo.objects.get(pk=id)
+            except ToDo.DoesNotExist:
+                raise NotFoundException
+            serializer = ToDoSerializer(TODO, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'msg': 'Partial Data Updated'})
+            return Response(serializer.errors)
+        return Response({'msg': 'U cannot update data because u do not put id'},
+                        status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    if request.method == 'DELETE':
+        id = pk
+        if id is not None:
+            try:
+                TODO = ToDo.objects.get(pk=id)
+            except ToDo.DoesNotExist:
+                raise NotFoundException
+            TODO.delete()
+            return Response({'msg' : 'Data Deleted'})
+        return Response({'msg': 'U cannot Delete data because u cannot put id'},
+                        status=status.HTTP_400_BAD_REQUEST)
 
-
-class ToDoSpecific(GenericAPIView, RetrieveModelMixin):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-
-class ToDoUpdate(GenericAPIView, UpdateModelMixin):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-
-class ToDoDelete(GenericAPIView, DestroyModelMixin):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-# class ToDoListCreate(GenericAPIView, ListModelMixin,CreateModelMixin):
-#     queryset = ToDo.objects.all()
-#     serializer_class = ToDoSerializer
-#
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request,*args,**kwargs)
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request,*args,**kwargs)
-#
-# class ToDoSpecificUpdateDelete(GenericAPIView,RetrieveModelMixin,UpdateModelMixin, DestroyModelMixin):
-#     queryset = ToDo.objects.all()
-#     serializer_class = ToDoSerializer
-#
-#     def get(self, request, *args, **kwargs):
-#         return self.retrieve(request,*args,**kwargs)
-#
-#     def put(self, request, *args, **kwargs):
-#         return self.update(request,*args,**kwargs)
-#
-#     def delete(self, request, *args, **kwargs):
-#         return self.destroy(request,*args,**kwargs)
--- todo_application/urls.py
+++ todo_application/urls.py
@@ -20,11 +20,7 @@ from todo_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/get_all_todo/', views.ToDoList.as_view()),
-    path('api/create_todo/', views.ToDoCreate.as_view()),
-    path('api/get_todo/<int:pk>/', views.ToDoSpecific.as_view()),
-    path('api/update_todo/<int:pk>/', views.ToDoUpdate.as_view()),
-    path('api/remove_todo/<int:pk>/', views.ToDoDelete.as_view()),
-    # path('apitodo/<int:pk>/', views.ToDoSpecificUpdateDelete.as_view()),
-    # path('apitodo/', views.ToDoListCreate.as_view()),
+    path('api/todoApp/', views.ToDo_Api),
+    path('api/todoApp/<int:pk>', views.ToDo_Api),
+
 ]
