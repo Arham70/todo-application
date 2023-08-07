455f1f3325f20170a77f610adc3acf3101578261
Arham <nasirarham656@gmail.com>
2023-08-07 16:11:43 -0700

    Exception file create to handle error, model and serializer use to uniqueness of our title.

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # Create your models here.
 class ToDo(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     created_at = models.DateTimeField(default=datetime.now())
     completed = models.BooleanField(default=False)
--- todo_app/serializer.py
+++ todo_app/serializer.py
@@ -6,3 +6,9 @@ class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
         fields = ['id', 'title', 'description', 'created_at', 'completed']
+
+    def validate_title(self, value):
+
+        if ToDo.objects.filter(title=value).exists():
+            raise serializers.ValidationError("This value for title already exists.")
+        return value
