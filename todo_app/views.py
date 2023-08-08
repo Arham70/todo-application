f4ab1b6482ca8ec3364a61ee0682cb2741a8d449
Arham <nasirarham656@gmail.com>
2023-08-07 17:51:21 -0700

    Update functional based api and install, implement swagger

--- todo_app/views.py
+++ todo_app/views.py
@@ -6,10 +6,11 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from .Exception import NotFoundException
 
-@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
-def ToDo_Api(request, pk=None):
-    if request.method =='GET':
-        id =pk
+
+@api_view(['GET'])
+def GetApi(request, pk=None):
+    if request.method == 'GET':
+        id = pk
         if id is not None:
             try:
                 TODO = ToDo.objects.get(id=id)
@@ -22,27 +23,36 @@ def ToDo_Api(request, pk=None):
         serializer = ToDoSerializer(TODO, many=True)
         return Response(serializer.data)
 
+
+@api_view(['POST'])
+def CreateApi(request):
     if request.method == 'POST':
         serializer = ToDoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg' : 'Data uploaded'}, status=status.HTTP_201_CREATED)
-        return Response({'msg' : 'Data not uploaded'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'msg': 'Data uploaded'}, status=status.HTTP_201_CREATED)
+        return Response({'msg': 'Data not uploaded'}, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['PUT'])
+def UpdateApi(request, pk=None):
     if request.method == 'PUT':
         id = pk
         if id is not None:
             try:
-                TODO= ToDo.objects.get(pk=id)
+                TODO = ToDo.objects.get(pk=id)
             except ToDo.DoesNotExist:
                 raise NotFoundException
-            serializer = ToDoSerializer(TODO, data=request.data , partial=False)
+            serializer = ToDoSerializer(TODO, data=request.data, partial=False)
             if serializer.is_valid():
                 serializer.save()
                 return Response({'msg': 'Complete Data Updated'})
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response({'msg': 'U cannot update data because u do not put id'}, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['PATCH'])
+def PartialUpdateApi(request, pk=None):
     if request.method == 'PATCH':
         id = pk
         if id is not None:
@@ -58,6 +68,9 @@ def ToDo_Api(request, pk=None):
         return Response({'msg': 'U cannot update data because u do not put id'},
                         status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['DELETE'])
+def DeleteApi(request, pk=None):
     if request.method == 'DELETE':
         id = pk
         if id is not None:
@@ -66,7 +79,6 @@ def ToDo_Api(request, pk=None):
             except ToDo.DoesNotExist:
                 raise NotFoundException
             TODO.delete()
-            return Response({'msg' : 'Data Deleted'})
+            return Response({'msg': 'Data Deleted'})
         return Response({'msg': 'U cannot Delete data because u cannot put id'},
                         status=status.HTTP_400_BAD_REQUEST)
-
--- todo_application/urls.py
+++ todo_application/urls.py
@@ -1,26 +1,31 @@
-"""
-URL configuration for todo_application project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from todo_app import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todo Application",
+        default_version='v1',
+        description="Test Swagger First app",
+        terms_of_service="http://www.ourapp.com/policies/terms/",
+        contact=openapi.Contact(email="contact@todo.local"),
+        license=openapi.License(name="Test License")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/todoApp/', views.ToDo_Api),
-    path('api/todoApp/<int:pk>', views.ToDo_Api),
-
+    path('api/GetTodo/', views.GetApi),
+    path('api/GetSpecificTodo/<int:pk>', views.GetApi),
+    path('api/CreateTodo/', views.CreateApi),
+    path('api/CompleteUpdateTodo/<int:pk>', views.UpdateApi),
+    path('api/PartialUpdateTodo/<int:pk>', views.PartialUpdateApi),
+    path('api/DeleteTodo/<int:pk>', views.DeleteApi),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
